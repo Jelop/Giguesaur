8924851381913cfdd899866c04c2b1598eff675f
Josh <joshua_lapine@hotmail.co.nz>
2015-09-09 17:27:32 +1200

    nothing works.. life is pain

--- findSquare.cpp
+++ findSquare.cpp
@@ -22,44 +22,46 @@ vector<blob> blobs;
 
 int findBlobs(Mat image){
 
+    cout << "in findBlobs" << endl;
     Mat temp;
     image.copyTo(temp);
+    cout << "about to enter for" << endl;
     for(int row = 0; row < temp.rows; row++){
         for(int col = 0; col < temp.cols; col++){
             if(temp.at<int>(row,col) == 0){
+                cout << "in if" << endl;
+                bool blob_exists = false;
                 for(int k = 0; k < blobs.size(); k++){
+                    cout << "iterating over blob vector" << endl;
                     if(row >= blobs[k].topleft.x && row <= blobs[k].botright.x &&
                        col >= blobs[k].topleft.y && col <= blobs[k].botright.y){
+                        cout << "added to existing blob" << endl;
                         blobs[k].row += row;
                         blobs[k].col += col;
                         blobs[k].count++;
+                        blob_exists = true;
                         break;
-                    } else if(k == blobs.size() - 1){
-                        //blob *blob1 = (blob *)malloc(sizeof (blob));
-                        //blob *blob1 = new blob;
+                    } 
+                }
 
+                if(!blob_exists){
 
-                        if(row - 10 < 0 || col - 75 < 0 ||
-                           row + 210 > res_y || col + 125 > res_x){
-                            return -1;
-                        }
-                        
-                        blobs.push_back(blob());
-                        
-                        blobs[k].topleft = Point2i(row - 10, col - 75);
-                        blobs[k].botright = Point2i(row + 210, col + 125);
-                        blobs[k].row = row;
-                        blobs[k].col = col;
-                        blobs[k].count = 1;
-
-                
-                        
-                        /* if(blob1->topleft.x < 0 || blob1->topleft.y < 0 ||
-                           blob1->botright.x > res_y || blob1->botright.y > res_x){
-                            delete blob1;
-                            return -1;
-                            }*/
+                    if(row - 10 < 0 || col - 75 < 0 ||
+                       row + 210 > res_y || col + 125 > res_x){
+                        return -1;
                     }
+                    
+                    cout << "creating new blob" << endl;
+                    blobs.push_back(blob());
+                    int index = blobs.size()-1;
+                    
+                    blobs[index].topleft = Point2i(row - 10, col - 75);
+                    blobs[index].botright = Point2i(row + 210, col + 125);
+                    blobs[index].row = row;
+                    blobs[index].col = col;
+                    blobs[index].count = 1;
+                    cout << "successfully created new blob" << endl;
+                    
                 }
             }
         }
@@ -133,9 +135,9 @@ int main( int argc, char** argv )
             return -1;
         }
 
-    Mat image ;
-    image = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);   // Read the file
-    image.convertTo(image, CV_8U);
+    Mat readin, image ;
+    readin = imread(argv[1], CV_LOAD_IMAGE_GRAYSCALE);   // Read the file
+    readin.convertTo(image, CV_8U);
     if(! image.data )                              // Check for invalid input
         {
             cout <<  "Could not open or find the image" << std::endl ;
@@ -144,34 +146,48 @@ int main( int argc, char** argv )
 
     //cvtColor(image,image, CV_RGB2GRAY);
     //GaussianBlur(image,image, Size(9, 9), 2, 2 );
-    /* for(int i = 0; i < image.rows; i++){
+    /*int pixel;
+    for(int i = 0; i < image.rows; i++){
         for(int j = 0; j < image.cols; j++){
-            int pixel = image.at<int>(i,j);
-            if(pixel < 0){
-                image.at<int>(i,j) = 255;
-            } else if (pixel > 0) {
+            //cout << "before pixel assignment" << endl;
+            if(i != 1078 && j != 1724)
+            pixel = image.at<int>(i,j);
+            //  printf("i = %d, j = %d\n", i,j);
+            if(pixel < 0 || pixel > 50){
+                // cout << "before if" << endl;
                 image.at<int>(i,j) = 255;
             } else {
-                cout << image.at<int>(i,j) << endl;
+                // cout<< "before else" << endl;
+                image.at<int>(i,j) = 0;
             }
         }
         }*/
-    threshold(image, image, 50, 255, THRESH_BINARY);
-    //imshow( "", image );
-    //waitKey(0);
-    
-    findBlobs(image);
 
-    for(int i = 0; i < blobs.size(); i++){
+    // threshold(image, image, 50, 255, THRESH_BINARY);
+    /*for(int i = 0; i < image.rows; i++){
+        for(int j = 0; j < image.cols; j++){
+            if(image.at<int>(i,j) != 0 && image.at<int>(i,j) != 255)
+                cout << image.at<int>(i,j) << endl;
+        }
+        }*/
+    //imshow( "", image );
+    // waitKey(0);
+    
+     findBlobs(image);
+
+    cout << "before cirlce drawing" << endl;
+     for(int i = 0; i < blobs.size(); i++){
         int x = blobs[i].col/blobs[i].count;
         int y = blobs[i].row/blobs[i].count;
+        cout << "circle " << i << " x = " << x << endl;
+        cout << "circle " << i << " y = " << y << endl;
       Point center(cvRound(x), cvRound(y));
       circle(image, center, 3, Scalar(255,0,0), -1, 8, 0);
       }
       //namedWindow( "", WINDOW_AUTOSIZE );// Create a window for display.
-      imshow( "", image );                   // Show our image inside it.
+     imshow( "", image );                   // Show our image inside it.
 
-      waitKey(0); // Wait for a keystroke in the window
+     waitKey(0); // Wait for a keystroke in the window
 
       //I dont think i have freed this memory
       /* for(int i = 0; i < blobs.size(); i++){
